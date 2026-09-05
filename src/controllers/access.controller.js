516ad38ab22a3d4ac0cec329c49a4b6acc6901c1
khanhvancong <khanhvc2310@gmail.com>
2026-09-05 11:29:47 +0700

    Refactor success response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,11 +1,17 @@
 'use strict'
 
-const accessController = require("../services/access.service");
 const accessService = require("../services/access.service");
+const { CREATED } = require("../core/success.response");
 
 class AccessController {
   signUp = async (req, res, next) => {
-    return res.status(201).json(await accessService.signUp(req.body));
+    new CREATED({
+      message: "Registerd OK!",
+      metadata: await accessService.signUp(req.body),
+      options: {
+        limit: 10
+      }
+    }).send(res)
   }
 }
 
